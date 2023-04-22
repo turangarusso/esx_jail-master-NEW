a430f3dab81280a3bf48e55199e3d56d4dff9567
Giovanni M Russo <113531412+turangarusso@users.noreply.github.com>
2023-04-23 01:33:59 +0200

    V1

---
+++ esx_jail-master/config.lua
@@ -0,0 +1,30 @@
+Config = {}
+Config.Locale = 'en'
+
+Config.JailBlip = vector3(1854.0, 2622.0, 45.0)
+Config.JailLocation = vector3(1665.3212, 2647.6428, 45.5527)
+Config.JailTimeSyncInterval = 60000 * 5
+
+
+Config.Uniforms = {
+	prison_wear = {
+		male = {
+			['tshirt_1'] = 15,  ['tshirt_2'] = 0,
+			['torso_1']  = 146, ['torso_2']  = 0,
+			['decals_1'] = 0,   ['decals_2'] = 0,
+			['arms']     = 0,   ['pants_1']  = 3,
+			['pants_2']  = 7,   ['shoes_1']  = 12,
+			['shoes_2']  = 12,  ['chain_1']  = 50,
+			['chain_2']  = 0
+		},
+		female = {
+			['tshirt_1'] = 3,   ['tshirt_2'] = 0,
+			['torso_1']  = 38,  ['torso_2']  = 3,
+			['decals_1'] = 0,   ['decals_2'] = 0,
+			['arms']     = 2,   ['pants_1']  = 3,
+			['pants_2']  = 15,  ['shoes_1']  = 66,
+			['shoes_2']  = 5,   ['chain_1']  = 0,
+			['chain_2']  = 2
+		}
+	}
+}
